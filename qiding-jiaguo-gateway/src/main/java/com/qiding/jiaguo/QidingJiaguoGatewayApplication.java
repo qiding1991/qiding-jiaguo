510b59d6a659cda69a957aadb59b27e211e6185f
qiding <qiding@syswin.com>
2020-11-08 16:46:01 +0800

    文件上传下载

--- qiding-jiaguo-gateway/src/main/java/com/qiding/jiaguo/QidingJiaguoGatewayApplication.java
+++ qiding-jiaguo-gateway/src/main/java/com/qiding/jiaguo/QidingJiaguoGatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import sun.jvm.hotspot.HelloWorld;
 
 @SpringBootApplication
 public class QidingJiaguoGatewayApplication {
